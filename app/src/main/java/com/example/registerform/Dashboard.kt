6d0d20ff13b2eeba15c2c8cc74ee31e149b3b633
duyenqa <ttemp8399@gmail.com>
2024-10-12 17:58:23 +0700

    update Dashboard screen

--- app/src/main/java/com/example/registerform/Dashboard.kt
+++ app/src/main/java/com/example/registerform/Dashboard.kt
@@ -2,6 +2,7 @@ package com.example.registerform
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,22 +15,48 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 
+data class Feature(val name: String)
 data class Item(val title: String, val icon: Int)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenDashboard() {
+    val oneList = listOf(
+        Feature(
+            name = "Sản phẩm 1"
+        ),
+        Feature(
+            name = "Sản phẩm 2"
+        ),
+        Feature(
+            name = "Sản phẩm 3"
+        ),
+        Feature(
+            name = "Sản phẩm 4"
+        ),
+        Feature(
+            name = "Sản phẩm 5"
+        ),
+        Feature(
+            name = "Sản phẩm 6"
+        )
+    )
     val ls = listOf(
         Item(
             title = "Text 1",
@@ -78,35 +105,99 @@ fun ScreenDashboard() {
             TopAppBar(title = { Text(text = "Screen Dashboard") })
         }
     ) { innerPadding ->
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-            .verticalScroll(rememberScrollState()),
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(15.dp)
         ) {
-            LazyColumn(
-                modifier = Modifier.height(800.dp),
-                verticalArrangement = Arrangement.spacedBy(15.dp)
+            //Content 1
+            Text(
+                text = "Ingredient 1",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(horizontal = 15.dp)
+            )
+            LazyVerticalGrid(
+                modifier = Modifier.height(300.dp).padding(horizontal = 15.dp),
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                items(ls, key = { it.title }) {
-                    Row(modifier = Modifier
-                        .fillMaxWidth()
+                items(items = oneList, key = { it.name }) {
+                    Box(modifier = Modifier
                         .clip(RoundedCornerShape(10.dp))
-                        .background(color = Color.LightGray)
-                        .padding(20.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(text = it.title,fontSize = 18.sp)
-                        Icon(
-                            painter = rememberAsyncImagePainter
-                                (model = R.drawable.baseline_arrow_forward_ios_24),
-                            "icon",
-                            modifier = Modifier.size(20.dp)
+                        .background(color = Color.Blue)
+                        .padding(10.dp)
+                    ){
+                        Text(
+                            text = it.name,
+                            fontSize = 18.sp,
+                            modifier = Modifier.padding(20.dp),
+                            color = Color.White
                         )
+                    }
+                }
+            }
+
+            //Content 2
+                Text(
+                    text = "Ingredient 2",
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(horizontal = 15.dp)
+                )
+                LazyRow(
+                    modifier = Modifier
+                        .padding(horizontal = 15.dp)
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    horizontalArrangement = Arrangement.spacedBy(15.dp)
+                ) {
+                    items(items = oneList, key = { it.name }) {
+                        Box(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(color = Color.Green)
+                        ) {
+                            Text(
+                                text = it.name,
+                                fontSize = 18.sp,
+                                modifier = Modifier.padding(20.dp),
+                                color = Color.White
+                            )
+                        }
+                    }
+                }
+                //Content 3
+                Text("Ingredient 3", fontSize = 20.sp, modifier = Modifier.padding(horizontal = 15.dp))
+                LazyColumn(
+                    modifier = Modifier.height(800.dp).padding(horizontal = 15.dp),
+                    verticalArrangement = Arrangement.spacedBy(15.dp)
+                ) {
+                    items(ls, key = { it.title }) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(color = Color.Magenta)
+                                .padding(20.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = it.title,
+                                fontSize = 18.sp,
+                                color = Color.White
+                            )
+                            Icon(
+                                painter = rememberAsyncImagePainter
+                                    (model = R.drawable.baseline_arrow_forward_ios_24),
+                                "icon",
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
                     }
                 }
             }
         }
     }
-}
